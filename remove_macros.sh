fec0ba17dca83036fcbd96771cdcd8068b602478
Luke Videckis <lukevideckis@gmail.com>
2024-04-22 16:14:32 -0600

    better method in script (don't create file chicken)

--- remove_macros.sh
+++ remove_macros.sh
@@ -6,18 +6,16 @@ git submodule init
 git submodule update
 
 for header in ./kactl/content/**/*.h; do
-  # in order to expand the kactl macros, you need to copy-paste the macros into
-  # the beginning of each .h file
-  cat kactl_macros.h "$header" > chicken
+  echo "$header"
   shortened_path="${header/\/kactl\/content/}"
   # need to create directory: below `cpp` command expects directory to exist
   mkdir -p $(dirname "$shortened_path")
-  cpp -std=c17 -nostdinc -C -P chicken "$shortened_path"
+  # in order to expand the kactl macros, you need to copy-paste the macros into
+  # the beginning of each .h file
+  cpp -std=c17 -nostdinc -C -P <(cat kactl_macros.h "$header") "$shortened_path"
 done
-
-rm chicken
 
 # the cpp preprocessor sometimes leaves blank empty lines
 sed --in-place '/^[[:space:]]*$/d' ./**/*.h
 # cpp command messes up formatting
-clang-format -i --style=file:.clang-format ./**/*.h ./**/*.cpp
+clang-format -i --style=file:.clang-format ./**/*.h
