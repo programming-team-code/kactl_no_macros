7f919a5edcc5ed7b4fb2cf7642077e7596ee452d
Luke Videckis <lukevideckis@gmail.com>
2024-04-22 16:01:43 -0600

    better way of doing it

--- remove_macros.sh
+++ remove_macros.sh
@@ -5,14 +5,14 @@ shopt -s globstar
 git submodule init
 git submodule update
 
-# to create all directories (cpp command below fails if destination path isn't created)
-cp -r ./kactl/content/. .
-
 for header in ./kactl/content/**/*.h; do
   # in order to expand the kactl macros, you need to copy-paste the macros into
   # the beginning of each .h file
   cat kactl_macros.h "$header" > chicken
-  cpp -std=c17 -nostdinc -C -P chicken "${header/\/kactl\/content/}"
+  shortened_path="${header/\/kactl\/content/}"
+  # need to create directory: below `cpp` command expects directory to exist
+  mkdir -p $(dirname "$shortened_path")
+  cpp -std=c17 -nostdinc -C -P chicken "$shortened_path"
 done
 
 rm chicken
