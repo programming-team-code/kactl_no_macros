b325099f67138fd519285e8a2bad34f19301823e
Luke Videckis <lukevideckis@gmail.com>
2024-04-22 16:42:29 -0600

    fix bug, and document what the bug was

--- remove_macros.sh
+++ remove_macros.sh
@@ -6,13 +6,21 @@ git submodule init
 git submodule update
 
 for header in ./kactl/content/**/*.h; do
-  echo "$header"
-  shortened_path="${header/\/kactl\/content/}"
-  # need to create directory: below `cpp` command expects directory to exist
-  mkdir -p $(dirname "$shortened_path")
-  # in order to expand the kactl macros, you need to copy-paste the macros into
-  # the beginning of each .h file
-  cpp -std=c17 -nostdinc -C -P <(cat kactl_macros.h "$header") "$shortened_path"
+  # explanation: something like
+  #
+  # cpp -std=c17 -nostdinc -C -P <(cat kactl_macros.h "$header")
+  #
+  # won't work when $header file includes some other file, because the cpp
+  # command can't find that included file (since cpp command reads output of
+  # cat from sub-shell, and doesn't know path of $header)
+  #
+  # So solution is to call cpp command twice: first with just $header as
+  # argument, so cpp command knows the path; now included files are copy-pasted
+  # into file.
+  #
+  # Then concatenate in kactl_macros.h and run cpp command again to expand
+  # macros.
+  cpp -std=c17 -nostdinc -C -P "$header" | cat kactl_macros.h - | cpp -std=c17 -nostdinc -C -P > "${header/\/kactl\/content/}"
 done
 
 # the cpp preprocessor sometimes leaves blank empty lines
